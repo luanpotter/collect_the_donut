d50e838616ba5d57d8a76109ce073f68d242fbca
Luan Nico <luanpotter27@gmail.com>
2024-09-14 12:16:52 -0400

    Add attack animation

--- lib/components/player.dart
+++ lib/components/player.dart
@@ -3,31 +3,51 @@ import 'dart:math';
 import 'package:collect_the_donut/collect_the_donut.dart';
 import 'package:collect_the_donut/input_utils.dart';
 import 'package:collect_the_donut/loader.dart';
-import 'package:flame/components.dart' show HasGameRef, KeyboardHandler;
+import 'package:flame/components.dart' as flame;
+import 'package:flame/events.dart';
 import 'package:flame/geometry.dart';
 import 'package:flame_3d/core.dart';
 import 'package:flame_3d_extras/model/model_component.dart';
 import 'package:flutter/services.dart';
 
 class Player extends ModelComponent
-    with HasGameRef<CollectTheDonutGame>, KeyboardHandler {
+    with
+        flame.HasGameRef<CollectTheDonutGame>,
+        flame.KeyboardHandler,
+        TapCallbacks {
   Player()
       : super(
           position: Vector3.zero(),
           model: Loader.models.rogue,
         ) {
-    final weapons = {
-      '1H_Crossbow',
-      '2H_Crossbow',
-      // 'Knife',
-      'Throwable',
-      'Knife_Offhand',
-    };
-    for (final weapon in weapons) {
-      hideNodeByName(weapon);
+    weapon = PlayerWeapon.knife;
+  }
+
+  PlayerAction? _action;
+  double _actionTimer = 0.0;
+  PlayerAction? get action => _action;
+  set action(PlayerAction? value) {
+    if (_actionTimer != 0.0) {
+      return;
     }
 
-    playAnimationByName('Idle');
+    _action = value;
+    _actionTimer = value?.timer ?? 0.0;
+    stopAnimation();
+  }
+
+  late PlayerWeapon _weapon;
+  PlayerWeapon get weapon => _weapon;
+  set weapon(PlayerWeapon value) {
+    _weapon = value;
+    _updateWeapon();
+  }
+
+  void _updateWeapon() {
+    for (final hide in PlayerWeapon.values) {
+      hideNodeByName(hide.nodeName);
+    }
+    hideNodeByName(weapon.nodeName, hidden: false);
   }
 
   double _lookAngle = 0.0;
@@ -45,12 +65,39 @@ class Player extends ModelComponent
   void update(double dt) {
     super.update(dt);
 
+    if (_actionTimer != 0.0) {
+      _actionTimer -= dt;
+      if (_actionTimer <= 0.0) {
+        _actionTimer = 0.0;
+        _action = null;
+      }
+    }
+
+    final isMoving = _handleMovement(dt);
+    _updateAnimation(isMoving: isMoving);
+  }
+
+  bool _handleMovement(double dt) {
+    if (_actionTimer != 0.0) {
+      return false;
+    }
+
     lookAngle += -_input.x * _rotationSpeed * dt;
 
     final movement = lookAt.scaled(-_input.y * _linearSpeed * dt);
     position.add(movement);
 
-    if (movement.length2 > 0.0) {
+    return movement.length2 > 0.0;
+  }
+
+  void _updateAnimation({required bool isMoving}) {
+    final action = _action;
+    if (action != null) {
+      switch (action) {
+        case PlayerAction.attack:
+          playAnimationByIdx(0, resetClock: false);
+      }
+    } else if (isMoving) {
       playAnimationByName('Walking_C', resetClock: false);
     } else {
       playAnimationByName('Idle', resetClock: false);
@@ -61,8 +108,38 @@ class Player extends ModelComponent
   bool onKeyEvent(KeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
     return readArrowLikeKeysIntoVector2(event, keysPressed, _input);
   }
+
+  @override
+  bool containsLocalPoint(flame.Vector2 point) => true;
+
+  @override
+  void onTapDown(_) {
+    action = PlayerAction.attack;
+  }
 }
 
 const double _rotationSpeed = 3.0;
 const double _linearSpeed = 2.5;
 final Vector3 _up = Vector3(0, 1, 0);
+
+enum PlayerAction {
+  attack(timer: 1.0666667222976685),
+  ;
+
+  final double timer;
+
+  const PlayerAction({required this.timer});
+}
+
+enum PlayerWeapon {
+  oneHandedCrossbow('1H_Crossbow'),
+  twoHandedCrossbow('2H_Crossbow'),
+  knife('Knife'),
+  throwable('Throwable'),
+  offhandKnife('Knife_Offhand'),
+  ;
+
+  final String nodeName;
+
+  const PlayerWeapon(this.nodeName);
+}
